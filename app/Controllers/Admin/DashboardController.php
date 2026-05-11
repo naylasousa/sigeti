858eba74f09ff0d0b1105ae664176ec16cdfdf4c
Náyla Gabrielle Soares de Sousa <gabriellesousasousa24@gmail.com>
2026-05-11 17:21:24 -0300

    feat(controller): criar o DashboardController do Admin

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -5,19 +5,38 @@ namespace App\Controllers\Admin;
 use App\Core\Auth;
 use App\Core\Controller;
 use App\Core\Permission;
+use App\Models\Department\Department;
+use App\Models\Role\Role;
 use App\Models\Ticket\Ticket;
+use App\Models\User;
+
 
 class DashboardController extends Controller
 {
     public function __construct()
     {
         parent::__construct("App");
-        Auth::requirePermission(Permission::VIEW_USERS);
+        Auth::requirePermission(Permission::VIEW_MANAGER_DASHBOARD);
     }
     public function index(): void
     {
-        $ticketModel = new Ticket();
-        $userId = Auth::user()->id;
+
+        $totalUsers = (new User())->totalUsers();
+        $recentUsers = (new User())->recentUsers();
+        $totalRoles = (new Role())->totalRoles();
+        $recentRoles = (new Role())->recentRoles();
+        $totalDepartments = (new Department())->totalDepartments();
+        $totalOpenTickets = (new Ticket())->totalOpenTickets();
+
+        echo $this->view->render("admin/dashboard", [
+            "totalUsers" => $totalUsers,
+            "recentUsers" => $recentUsers,
+            "totalRoles" => $totalRoles,
+            "recentRoles" => $recentRoles,
+            "totalDepartments" => $totalDepartments,
+            "totalOpenTickets" => $totalOpenTickets,
+
+        ]);
 
     }
 
