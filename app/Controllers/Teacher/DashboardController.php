c8222d0025cbdde52c41681190a0ce90c74d0f38
Náyla Gabrielle Soares de Sousa <gabriellesousasousa24@gmail.com>
2026-05-11 14:30:54 -0300

    fix(controllers): corrigir o nome da permição no chamado do método requirePermission() no controlador Teacher\Dashboard

--- app/Controllers/Teacher/DashboardController.php
+++ app/Controllers/Teacher/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
     {
         parent::__construct("App");
 
-        Auth::requireRole(Permission::VIEW_REQUESTER_DASHBOARD);
+        Auth::requirePermission(Permission::VIEW_REQUESTER_DASHBOARD);
     }
 
     public function index(): void
